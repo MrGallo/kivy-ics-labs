aa785bc297510bac6e41db0042a74faa4ce26473
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2021-07-07 19:52:10 -0400

    Initialize password checker lab

---
+++ 02-password-checker/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+
+
+class MainContainer(BoxLayout):
+    pass
+
+
+class PasswordCheckerApp(App):
+    pass
+
+
+PasswordCheckerApp().run()
