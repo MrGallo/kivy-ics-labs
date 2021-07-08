d53c38ecc969c4db2a92658adba1fc301d93e14a
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2021-07-07 20:17:26 -0400

    Add check button event handler

--- 02-password-checker/main.py
+++ 02-password-checker/main.py
@@ -6,6 +6,9 @@ from kivy.properties import StringProperty
 class MainContainer(BoxLayout):
     status_text = StringProperty("Enter a password")
 
+    def handle_check_button_press(self):
+        print("Check button was pressed!")
+
 
 class PasswordCheckerApp(App):
     pass
