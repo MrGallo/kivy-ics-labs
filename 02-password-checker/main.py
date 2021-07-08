0cd0fd28f093d9c13b464e517e1254092a2dd824
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2021-07-07 20:41:48 -0400

    Create the calculate score method

--- 02-password-checker/main.py
+++ 02-password-checker/main.py
@@ -10,8 +10,27 @@ class MainContainer(BoxLayout):
         print("Check button was pressed!")
         print(f"Password: {password}")
 
+        score = self.calc_password_score(password)
+        print(f"Score: {score}")
+
+        self.status_text = "Weak"
+
     def handle_info_button_press(self):
         print("Info button was pressed!")
+    
+    def calc_password_score(self, password):
+        """
+        The password will get 10pts for each criteria present:
+        - length > 8
+        - contains lower-case letters
+        - contains upper-case letters
+        - contains numbers
+        - contains symbols
+
+        A password cannot contain a space or be less than 6 characters. 
+        If so, return -1 (invalid).
+        """
+        return 0
 
 
 class PasswordCheckerApp(App):
